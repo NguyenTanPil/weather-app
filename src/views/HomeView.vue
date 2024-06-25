8244363d3b8de7124e69d55019908b5c4fa0390d
NguyenTanPil <traila1008@gmail.com>
2024-06-25 23:31:01 +0700

    Add the search place

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,58 @@
-<script setup></script>
-
 <template>
-  <main>home</main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxResults"
+      >
+        <p v-if="searchError">Sorry, something went wrong, please try again!</p>
+
+        <p v-if="mapboxResults.length === 0">No results match your query, try a different term!</p>
+        <template v-else>
+          <li v-for="item in mapboxResults" :key="item.id" class="py-2 cursor-pointer">
+            {{ item.place_name }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const mapboxAPIKey =
+  'pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q';
+const searchQuery = ref('');
+const queryTimeout = ref(null);
+const mapboxResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`
+        );
+        mapboxResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
+    } else {
+      mapboxResults.value = null;
+    }
+  }, 300);
+};
+</script>
