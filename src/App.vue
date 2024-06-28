c115136ce2cc19087f976b7053734dddef97912e
NguyenTanPil <traila1008@gmail.com>
2024-06-28 23:23:11 +0700

    Add the title page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
-<script setup>
-import SiteNavigation from './components/SiteNavigation.vue';
-</script>
-
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView />
+    <RouterView #="{ Component }" class="flex-1">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
+
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
 
 <style scoped>
 header {
@@ -70,5 +74,13 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+}
+
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,25 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Home'
+      }
     },
     {
       path: '/weather/:state/:city',
       name: 'city',
-      component: CityView
+      component: CityView,
+      meta: {
+        title: 'Weather'
+      }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.params.city ? `${to.params.city}, ${to.params.state}` : to.meta.title} | Local Weather`;
+  next();
 });
 
 export default router;
